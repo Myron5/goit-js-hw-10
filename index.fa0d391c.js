ec7ac606e535edbc88a7db72b0d7efae377a15da
Myron5 <96477512+Myron5@users.noreply.github.com>
2023-01-15 14:57:52 +0000

    Deploying to gh-pages from @ Myron5/goit-js-hw-10@c1b7cef9a472afe7d6e1f92b97133fa095c0f2a6 🚀

---
+++ index.fa0d391c.js
@@ -0,0 +1,2 @@
+!function(){var n;(n="peru",fetch("https://restcountries.com/v3.1/name/".concat(n)).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()})).catch((function(n){return console.log(n)}))).then((function(n){return console.log(n)})),console.log("Commit")}();
+//# sourceMappingURL=index.fa0d391c.js.map
