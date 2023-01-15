c1b7cef9a472afe7d6e1f92b97133fa095c0f2a6
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-01-15 16:57:19 +0200

    com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(resp => {
+      if (!resp.ok) throw new Error(resp.status);
+      return resp.json();
+    })
+    .catch(err => console.log(err));
+}
+
+fetchCountries('peru').then(data => console.log(data));
+
+console.log('Commit');
